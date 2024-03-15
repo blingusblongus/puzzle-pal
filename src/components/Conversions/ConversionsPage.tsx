eba8f1f29c660380bda7dc03d95a4233d776a942
Nick Allen <rutherford.t.b@gmail.com>
2024-03-14 20:10:47 -0500

    add basic binary translator

--- src/components/Conversions/ConversionsPage.tsx
+++ src/components/Conversions/ConversionsPage.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
 export const ConversionsPage = () => {
-  return <div>Conversions</div>;
+  const [fromValue, setFromValue] = useState("");
+
+  const KeypadButton = ({ num }: { num: number }) => {
+    return (
+      <Button
+        value={num}
+        onClick={() => setFromValue(fromValue + num.toString())}
+      >
+        {num}
+      </Button>
+    );
+  };
+
+  const ClearButton = () => {
+    return <Button onClick={() => setFromValue("")}>C</Button>;
+  };
+
+  console.log(fromValue);
+
+  return (
+    <div>
+      <Input value={fromValue} readOnly className="bg-slate-400 text-right" />
+      <div className="flex gap-1 py-1">
+        <KeypadButton num={0} />
+        <KeypadButton num={1} />
+        <ClearButton />
+      </div>
+      <div>Result: {parseInt(fromValue, 2)}</div>
+    </div>
+  );
 };
 
 export const intToBin = (int: number): string => {
