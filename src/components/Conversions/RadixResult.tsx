6140dc850e88b1ad93155b54632ea76ee6b22c12
Nick Allen <rutherford.t.b@gmail.com>
2024-03-18 13:19:10 -0500

    prevent selecting labels

--- src/components/Conversions/RadixResult.tsx
+++ src/components/Conversions/RadixResult.tsx
@@ -6,7 +6,7 @@ type RadixResult = {
 export const RadixResult = ({ result, label }: RadixResult) => {
   return (
     <div className="flex h-full w-full flex-col-reverse items-center justify-center rounded-xl border-2 border-neutral-400 p-3">
-      <div>{label}</div>
+      <div className="select-none">{label}</div>
       <div className="flex w-full grow items-center justify-center overflow-x-scroll text-2xl">
         {result}
       </div>
