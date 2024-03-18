eac29bc872487fb2720432d13cce43e653199932
Nick Allen <rutherford.t.b@gmail.com>
2024-03-18 12:00:35 -0500

    add select element with specific numInputs

--- src/components/Conversions/ConversionsPage.tsx
+++ src/components/Conversions/ConversionsPage.tsx
@@ -1,14 +1,49 @@
 import { useState } from "react";
 import { Input } from "../ui/input";
 import { Keypad } from "./Keypad";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export const ConversionsPage = () => {
+  const [fromRadix, setFromRadix] = useState(2);
   const [fromValue, setFromValue] = useState("");
-  const base10 = parseInt(fromValue, 2) || 0;
+  const base10 = parseInt(fromValue, fromRadix) || 0;
 
   return (
     <div className="flex h-full w-full flex-col gap-8">
       <div>
+        <div className="flex items-center gap-4">
+          <div className="font-bold">Input Radix:</div>
+          <div className="w-40">
+            <Select
+              defaultValue="2"
+              onValueChange={(v) => {
+                setFromValue(base10.toString(parseInt(v)));
+                setFromRadix(parseInt(v));
+              }}
+            >
+              <SelectTrigger className="bg-primary-foreground">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Radix</SelectLabel>
+                  <SelectItem value="2">Binary (2)</SelectItem>
+                  <SelectItem value="10">Decimal (10)</SelectItem>
+                  <SelectItem value="16">Hexadecimal (16)</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
         <div className="flex items-end justify-between">
           <span>Result: </span>
           <span className="text-2xl font-bold">{base10}</span>
@@ -23,7 +58,11 @@ export const ConversionsPage = () => {
         <div>Hexadecimal: {base10.toString(16)}</div>
       </div>
 
-      <Keypad radix={2} setter={setFromValue} displayValue={fromValue} />
+      <Keypad
+        radix={fromRadix}
+        setter={setFromValue}
+        displayValue={fromValue}
+      />
     </div>
   );
 };
