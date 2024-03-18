1a517feea0f317bc8fd899dd3a46013cadea58b2
Nick Allen <rutherford.t.b@gmail.com>
2024-03-17 19:54:02 -0500

    move keypad and add b10 and hex variants

--- src/components/Conversions/ConversionsPage.tsx
+++ src/components/Conversions/ConversionsPage.tsx
@@ -1,47 +1,20 @@
 import { useState } from "react";
 import { Input } from "../ui/input";
-import { Button } from "../ui/button";
 import { Keypad } from "./Keypad";
 
 export const ConversionsPage = () => {
   const [fromValue, setFromValue] = useState("");
-
-  const KeypadButton = ({ num }: { num: number }) => {
-    return (
-      <Button
-        value={num}
-        onClick={() => setFromValue(fromValue + num.toString())}
-        size="lg"
-      >
-        {num}
-      </Button>
-    );
-  };
-
-  const ClearButton = () => {
-    return (
-      <Button size="lg" onClick={() => setFromValue("")}>
-        C
-      </Button>
-    );
-  };
-
   const base10 = parseInt(fromValue, 2) || 0;
 
   return (
-    <div className="flex h-full w-full flex-col justify-between">
-      <div className="bg-green-200">
+    <div className="flex h-full w-full flex-col gap-8">
+      <div>
         <div className="flex items-end justify-between">
           <span>Result: </span>
           <span className="text-2xl font-bold">{base10}</span>
         </div>
 
         <Input value={fromValue} readOnly className="bg-slate-700 text-right" />
-        <div className="flex gap-1 py-1">
-          <KeypadButton num={0} />
-          <KeypadButton num={1} />
-          <ClearButton />
-        </div>
 
         <div>Base10: {base10 || 0}</div>
         <div>Base2: {base10.toString(2)}</div>
--- src/components/Conversions/Keypad.tsx
+++ src/components/Conversions/Keypad.tsx
@@ -6,7 +6,7 @@ type KeypadProps = {
   setter: (value: React.SetStateAction<string>) => void;
 };
 export const Keypad = ({ radix, setter, displayValue }: KeypadProps) => {
-  const KeypadButton = ({ num }: { num: number }) => {
+  const KeypadButton = ({ num }: { num: number | string }) => {
     return (
       <Button
         value={num}
@@ -29,10 +29,56 @@ export const Keypad = ({ radix, setter, displayValue }: KeypadProps) => {
   switch (radix) {
     case 2:
       return (
-        <div className="mb-16 flex w-full flex-col gap-4 bg-purple-950">
+        <div className="flex w-full max-w-sm flex-col gap-4">
           <div className="grid grid-cols-2 gap-4">
             <KeypadButton num={0} />
             <KeypadButton num={1} />
+          </div>
+          <ClearButton />
+        </div>
+      );
+    case 10:
+      return (
+        <div className="flex w-full max-w-sm flex-col gap-4">
+          <div className="grid grid-cols-3 gap-4">
+            <KeypadButton num={1} />
+            <KeypadButton num={2} />
+            <KeypadButton num={3} />
+            <KeypadButton num={4} />
+            <KeypadButton num={5} />
+            <KeypadButton num={6} />
+            <KeypadButton num={7} />
+            <KeypadButton num={8} />
+            <KeypadButton num={9} />
+            <div />
+            <KeypadButton num={0} />
+            <div />
+          </div>
+          <ClearButton />
+        </div>
+      );
+    case 16:
+      return (
+        <div className="flex w-full max-w-sm flex-col gap-4">
+          <div className="grid grid-cols-3 gap-4">
+            <KeypadButton num={1} />
+            <KeypadButton num={2} />
+            <KeypadButton num={3} />
+            <KeypadButton num={4} />
+            <KeypadButton num={5} />
+            <KeypadButton num={6} />
+            <KeypadButton num={7} />
+            <KeypadButton num={8} />
+            <KeypadButton num={9} />
+            <KeypadButton num={"a"} />
+            <KeypadButton num={"b"} />
+            <KeypadButton num={"c"} />
+            <KeypadButton num={"d"} />
+            <KeypadButton num={"e"} />
+            <KeypadButton num={"f"} />
+            <div />
+            <KeypadButton num={0} />
+            <div />
           </div>
           <ClearButton />
         </div>
