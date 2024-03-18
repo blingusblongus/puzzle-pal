6406c404717e92fd16d896beb5191028e9259290
Nick Allen <rutherford.t.b@gmail.com>
2024-03-18 14:29:48 -0500

    add basic morse inputs

--- src/components/MorseTranslator/MorseTranslator.tsx
+++ src/components/MorseTranslator/MorseTranslator.tsx
@@ -1,3 +1,60 @@
+import { Dot, Minus, MoveDown, MoveLeft, Space } from "lucide-react";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import { useState } from "react";
+
 export const MorseTranslator = () => {
-  return <div>Morse</div>;
+  const [inputText, setInputText] = useState("");
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col items-center gap-4">
+        <div>Input:</div>
+        <div className="flex flex-wrap rounded-lg border border-neutral-600 p-3">
+          {inputText.split("").map((char) => {
+            switch (char) {
+              case ".":
+                return <Dot size={10} />;
+              case "-":
+                return <Minus size={10} />;
+              case " ":
+                return <span className="w-3" />;
+              case "/":
+                return <Space size={16} />;
+              default:
+                return char;
+            }
+          })}
+        </div>
+        {/* <Textarea className="border-neutral-600" value={inputText} /> */}
+        <MoveDown />
+        <div>Output:</div>
+        <Textarea className="border-neutral-600" value={"Output Text"} />
+      </div>
+      <div className="flex justify-center gap-4">
+        <Button onClick={() => setInputText((prev) => prev + ".")}>
+          <Dot />
+        </Button>
+        <Button onClick={() => setInputText((prev) => prev + "-")}>
+          <Minus />
+        </Button>
+        <Button
+          onClick={() => setInputText((prev) => prev.slice(0, prev.length - 1))}
+        >
+          <MoveLeft />
+        </Button>
+      </div>
+      <div className="flex justify-center gap-4">
+        <Button onClick={() => setInputText((prev) => prev + " ")}>
+          Next Letter
+        </Button>
+        <Button onClick={() => setInputText((prev) => prev + "/")}>
+          Next Word
+        </Button>
+      </div>
+      <div className="flex justify-center gap-4">
+        <Button onClick={() => setInputText("")}>Clear</Button>
+      </div>
+    </div>
+  );
 };
