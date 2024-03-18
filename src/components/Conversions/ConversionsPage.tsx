002c91b551d9cadd7f975e0fe617d649e3268107
Nick Allen <rutherford.t.b@gmail.com>
2024-03-18 12:52:31 -0500

    rm unused tests and functions

--- src/components/Conversions/ConversionsPage.tsx
+++ src/components/Conversions/ConversionsPage.tsx
@@ -71,11 +71,3 @@ export const ConversionsPage = () => {
     </div>
   );
 };
-
-export const intToBin = (int: number): string => {
-  return int.toString(2);
-};
-
-export const binToInt = (bin: string): number => {
-  return parseInt(bin, 2);
-};
